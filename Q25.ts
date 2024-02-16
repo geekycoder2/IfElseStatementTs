9f1f9352d1ba3940b796bc6c6a82c2165c7736ba
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-17 01:56:56 +0500

    Add files via upload

---
+++ Q25.ts
@@ -0,0 +1,18 @@
+// Q25.Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+
+// Declare a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+const alien_color1: string = 'green';
+
+// Write an if statement to test whether the alien's color is green.
+if (alien_color1 === 'green') {
+    // If it is green, print a message that the player just earned 5 points.
+    console.log('Player just earned 5 points!');
+} else {
+    // If it is not green, you can optionally provide an alternative message.
+    console.log('No points earned. Alien color is not green.');
+}
+
